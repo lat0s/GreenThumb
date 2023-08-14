82167d000311d72265b1385da5163ebcf5c3e714
lat0s <latosgeorge@gmail.com>
2023-08-14 17:34:26 +0200

    Code Smell 2# Remove this unused import 'androidx.annotation.NonNull'.               Unnecessary imports should be removedjava:S1128

--- group-2-android/app/src/main/java/com/example/notgreenthumb/Dashboard.java
+++ group-2-android/app/src/main/java/com/example/notgreenthumb/Dashboard.java
@@ -1,5 +1,4 @@
 package com.example.notgreenthumb;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
